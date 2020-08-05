001fe94d5189167ef5b0c9a775b3e78e85d3a7d1
DaftonJ <dawid@MacBook-Pro-Dawid.local>
2020-08-05 13:41:05 +0200

    Final working project

---
+++ RejestrOsobZaginionych/AuthorizeWithRoleAttribute.cs
@@ -0,0 +1,21 @@
+﻿using RejestrOsobZaginionych.Enums;
+using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RejestrOsobZaginionych.Helpers
+{
+    public class AuthorizeWithRoleAttribute : AuthorizeAttribute
+    {
+        private readonly Roles _role;
+
+        public AuthorizeWithRoleAttribute(Roles role)
+        {
+            _role = role;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext) => httpContext.User.HasPermissions(_role);
+
+    }
+}
---
+++ RejestrOsobZaginionych/Genders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RejestrOsobZaginionych.Enums
+{
+    public enum Genders
+    {
+        Meżczyzna,
+        Kobieta
+    }
+}
